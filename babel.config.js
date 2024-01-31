c20f7803cd1fa5bf744f3bdcb4e209f5d787eeb4
Luiz Henrique <luizhjramos@outlook.com>
2024-01-31 20:52:56 -0300

    chore: configure alias import

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@': './src/*',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+  ],
 };
