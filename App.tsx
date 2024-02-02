88b9d02beea993078ddbe4125ff74dd06dc5a340
Luiz Henrique <luizhjramos@outlook.com>
2024-02-01 23:57:03 -0300

    feat: add EyeOnIcon component

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import {theme} from '@theme';
 import {EyeOffIcon} from '@assets/icons/EyeOffIcon';
 
 import {Text} from '@components/Text';
+import {EyeOnIcon} from '@assets/icons/EyeOnIcon';
 
 const App = (): React.JSX.Element => (
   <ThemeProvider theme={theme}>
@@ -14,6 +15,7 @@ const App = (): React.JSX.Element => (
         <Text preset="paragraphSmall">Nubble App</Text>
 
         <EyeOffIcon color="blue" />
+        <EyeOnIcon color="red" />
       </View>
     </SafeAreaView>
   </ThemeProvider>
