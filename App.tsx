c11ecfd967f8bb78b3f83032f105edf25fbb5a97
Luiz Henrique <luizhjramos@outlook.com>
2024-02-01 23:54:02 -0300

    feat: add EyeOffIcon component

--- App.tsx
+++ App.tsx
@@ -3,24 +3,17 @@ import {SafeAreaView, View} from 'react-native';
 import {ThemeProvider} from '@shopify/restyle';
 
 import {theme} from '@theme';
+import {EyeOffIcon} from '@assets/icons/EyeOffIcon';
 
-import {Box} from '@components/Box';
 import {Text} from '@components/Text';
-import {Button} from '@components/Button';
 
 const App = (): React.JSX.Element => (
   <ThemeProvider theme={theme}>
     <SafeAreaView>
-      <View style={{padding: 20}}>
+      <View style={{paddingHorizontal: 20}}>
         <Text preset="paragraphSmall">Nubble App</Text>
 
-        <Box marginBottom="s24">
-          <Button title="Entrar" disabled />
-        </Box>
-        <Box marginBottom="s24">
-          <Button preset="outline" title="Outlier" />
-        </Box>
-        <Button title="Loading" isLoading />
+        <EyeOffIcon color="blue" />
       </View>
     </SafeAreaView>
   </ThemeProvider>
