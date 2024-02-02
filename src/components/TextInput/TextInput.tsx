78fffda68f977a55d7f5adb6872821d8729ae93a
Luiz Henrique <luizhjramos@outlook.com>
2024-02-02 13:31:08 -0300

    styles: TextInput

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import {TextInput as RNTextInput, TextInputProps} from 'react-native';
+import {
+  TextInput as RNTextInput,
+  TextInputProps,
+  TextStyle,
+} from 'react-native';
 
 import {Box, BoxProps} from '@components/Box';
 import {Text} from '@components/Text';
+import {$fontFamily, $fontSizes} from '@components/Text/utils';
+import {useAppTheme} from '@/hooks/useAppTheme';
 
 interface ITextInputProps extends TextInputProps {
   label: string;
 }
 
-export const TextInput = ({label, ...rest}: ITextInputProps) => (
-  <Box>
-    <Text preset="paragraphMedium" mb="s4">
-      {label}
-    </Text>
-    <Box {...$textInputContainer}>
-      <RNTextInput
-        {...rest}
-        style={{height: 50, borderWidth: 1, borderRadius: 8, padding: 16}}
-      />
+export const TextInput = ({label, ...rest}: ITextInputProps) => {
+  const {colors} = useAppTheme();
+
+  return (
+    <Box>
+      <Text preset="paragraphMedium" mb="s4">
+        {label}
+      </Text>
+      <Box {...$textInputContainer}>
+        <RNTextInput
+          placeholderTextColor={colors.gray2}
+          style={$textInputStyles}
+          {...rest}
+        />
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
+
+const $textInputStyles: TextStyle = {
+  borderWidth: 1,
+  padding: 0,
+  fontFamily: $fontFamily.regular,
+  ...$fontSizes.paragraphMedium,
+};
 
 const $textInputContainer: BoxProps = {
   borderWidth: 1,
