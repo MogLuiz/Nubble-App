b3df543b0bc061dd51957728df77a3a6282ceb00
Luiz Henrique <luizhjramos@outlook.com>
2024-02-05 10:11:00 -0300

    feat: add canGoBack Screen component variant

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 
 import {Box} from '@components/Box';
+import {Icon} from '@components/Icon';
+import {Text} from '@components/Text';
 
 import {useAppSafeArea} from '@hooks/useAppSafeArea';
 
 interface ILoginScreenProps {
   children?: React.ReactNode;
+  canGoBack?: boolean;
 }
 
-export const Screen = ({children}: ILoginScreenProps) => {
+export const Screen = ({children, canGoBack = false}: ILoginScreenProps) => {
   const {topSpacing: paddingTop} = useAppSafeArea();
 
   return (
     <Box paddingHorizontal="s24" style={{paddingTop}}>
+      {canGoBack && (
+        <Box mb="s24" flexDirection="row" alignItems="center" gap="s8">
+          <Icon variant="arrowLeft" color="primary" />
+          <Text preset="paragraphMedium" semiBold>
+            Voltar
+          </Text>
+        </Box>
+      )}
+
       {children}
     </Box>
   );
