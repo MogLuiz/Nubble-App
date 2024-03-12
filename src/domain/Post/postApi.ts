df839a615b910ada974cae0dd14d3f342e49d5ea
Luiz Henrique <luizhjramos@outlook.com>
2024-03-12 20:53:23 -0300

    feat: add post api layer

---
+++ src/domain/Post/postApi.ts
@@ -0,0 +1,14 @@
+import {IPost} from './types';
+import {postListMock} from './postMock';
+
+const list = (): Promise<IPost[]> => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(postListMock);
+    }, 1000);
+  });
+};
+
+export const postApi = {
+  list,
+};
