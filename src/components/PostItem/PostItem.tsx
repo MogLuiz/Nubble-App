4dcd0ee7679fc2695f73039b22a76f0304f1fffb
Luiz Henrique <luizhjramos@outlook.com>
2024-03-13 09:56:51 -0300

    feat: add Post actions component

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -5,13 +5,21 @@ import {IPost} from '@domain/Post';
 import {Box} from '@components/Box';
 import {PostImage} from './parts/PostImage';
 import {PostHeader} from './parts/PostHeader';
+import {PostActions} from './parts/PostActions';
 
 interface IPostItemProps {
   post: IPost;
 }
-export const PostItem = ({post: {author, imageURL}}: IPostItemProps) => (
+export const PostItem = ({
+  post: {author, imageURL, commentCount, favoriteCount, reactionCount},
+}: IPostItemProps) => (
   <Box marginBottom="s24">
     <PostHeader author={author} />
     <PostImage imageURL={imageURL} />
+    <PostActions
+      commentCount={commentCount}
+      favoriteCount={favoriteCount}
+      reactionCount={reactionCount}
+    />
   </Box>
 );
--- src/components/PostItem/parts/PostHeader.tsx
+++ src/components/PostItem/parts/PostHeader.tsx
@@ -8,14 +8,16 @@ import {Text} from '@components/Text';
 
 type PostHeaderProps = Pick<IPost, 'author'>;
 
-export const PostHeader = ({author}: PostHeaderProps) => (
+export const PostHeader = ({
+  author: {profileURL, userName},
+}: PostHeaderProps) => (
   <Box flexDirection="row" alignItems="center" mb="s16">
     <Image
-      source={{uri: author.profileURL}}
+      source={{uri: profileURL}}
       style={{width: 32, height: 32, borderRadius: 14}}
     />
     <Text ml="s12" semiBold preset="paragraphMedium">
-      {author.userName}
+      {userName}
     </Text>
   </Box>
 );
