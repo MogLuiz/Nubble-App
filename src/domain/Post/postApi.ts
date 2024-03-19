80b929a86d7e83c283b1f0894c0c394bf3ff765d
Luiz Henrique <luizhjramos@outlook.com>
2024-03-19 00:05:05 -0300

    feat: fetch users data

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,12 +1,19 @@
 import {IPost} from '.';
 import {postListMock} from './postMock';
 
-const list = (): Promise<IPost[]> => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(postListMock);
-    }, 1000);
+const list = async (): Promise<IPost[]> => {
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer Ng.-3Jn5LvR3Tg22Tik13GNGynNQgtkFlOOAvBt2kHqooq2kNyyy73Qv0l_WZsa',
+    },
   });
+
+  let data = await response.json();
+  console.log('FETCH DATA:', data);
+
+  return postListMock;
 };
 
 export const postApi = {
