627ee638a80725a50c3c5ec91e353f2439602755
Luiz Henrique <luizhjramos@outlook.com>
2024-03-19 22:05:51 -0300

    refactor: change api comunication from fetch to axios

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -1,19 +1,12 @@
 import { PostAPI } from '.';
-import { PaginatedResponseAPI } from '@api';
+import { api, PaginatedResponseAPI } from '@api';
 
-const list = async (): Promise<PaginatedResponseAPI<PostAPI>> => {
-  let response = await fetch('http://localhost:3333/user/post', {
-    method: 'GET',
-    headers: {
-      Authorization:
-        'Bearer Ng.-3Jn5LvR3Tg22Tik13GNGynNQgtkFlOOAvBt2kHqooq2kNyyy73Qv0l_WZsa',
-    },
-  });
+const list = async () => {
+  const { data } = await api.get<PaginatedResponseAPI<PostAPI>>('/user/post');
 
-  let data: PaginatedResponseAPI<PostAPI> = await response.json();
   return data;
 };
 
 export const postApi = {
-  list,
+  list
 };
