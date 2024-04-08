ddb3d30a98aa4fb5d6561d7309be8e32cbf434e1
Luiz Henrique <luizhjramos@outlook.com>
2024-04-08 18:14:18 -0300

    feat: add create post comment api layer

--- src/domain/PostComment/postCommentApi.ts
+++ src/domain/PostComment/postCommentApi.ts
@@ -2,16 +2,29 @@ import { PostCommentAPI } from '@domain/PostComment/postCommentTypes';
 import { api, PaginatedResponseAPI, PaginationParams } from '@api';
 
 const list = async (post_id: number, pagination_params?: PaginationParams) => {
-  const { data } = await api.get<PaginatedResponseAPI<PostCommentAPI>>('user/post_comment', {
-    params: {
-      post_id,
-      ...pagination_params
-    }
+  const { data } = await api.get<PaginatedResponseAPI<PostCommentAPI>>(
+    'user/post_comment',
+    {
+      params: {
+        post_id,
+        ...pagination_params,
+      },
+    },
+  );
+
+  return data;
+};
+
+const create = async (post_id: number, comment: string) => {
+  const { data } = await api.post<PostCommentAPI>('user/post_comment', {
+    post_id,
+    comment,
   });
 
   return data;
 };
 
 export const postCommentApi = {
-  list
+  list,
+  create,
 };
