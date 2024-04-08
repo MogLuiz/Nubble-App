46ada37ad8941bf8e901e498fac088a777ab9189
Luiz Henrique <luizhjramos@outlook.com>
2024-04-08 18:21:18 -0300

    feat: add usePostCommentCreate custom hook to implement post comment creation use case

---
+++ src/domain/PostComment/useCases/usePostCommentCreate.ts
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+import { postCommentService } from '@domain/PostComment';
+
+export const usePostCommentCreate = (post_id: number) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<boolean | null>(null);
+
+  const createPostComment = async (comment: string) => {
+    try {
+      setLoading(true);
+      setError(null);
+      await postCommentService.create(post_id, comment);
+    } catch (e) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    createPostComment,
+    loading,
+    error,
+  };
+};
