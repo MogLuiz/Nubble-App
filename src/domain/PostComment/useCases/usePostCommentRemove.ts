e5c4ef03364c94d11a62530a335a9dc8881513a2
Luiz Henrique <luizhjramos@outlook.com>
2024-04-28 12:42:33 -0300

    feat: add post comment remove use case

--- src/domain/PostComment/useCases/usePostCommentRemove.ts
+++ src/domain/PostComment/useCases/usePostCommentRemove.ts
@@ -1,1 +1,7 @@
-export const x = 1;
+import {useMutation, MutationOptions} from '@infra/hooks/useMutation';
+
+import {postCommentService} from '@domain/PostComment';
+
+export const usePostCommentRemove = (options?: MutationOptions<string>) => {
+  return useMutation<number, string>(postCommentService.remove, options);
+};
