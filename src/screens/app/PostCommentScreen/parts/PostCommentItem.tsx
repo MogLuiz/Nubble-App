8eaa97f32ea5acbd646724c3c627a0010582ceb1
Luiz Henrique <luizhjramos@outlook.com>
2024-04-29 10:23:35 -0300

    feat: remove post comment UI integration

--- src/screens/app/PostCommentScreen/parts/PostCommentItem.tsx
+++ src/screens/app/PostCommentScreen/parts/PostCommentItem.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
+import {Alert, Pressable} from 'react-native';
 
 import {Box} from '@components/Box';
 import {Text} from '@components/Text';
 import {ProfileAvatar} from '@components/ProfileAvatar';
 
 import {PostComment} from '@/domain/PostComment';
+import {usePostCommentRemove} from '@domain/PostComment/useCases/usePostCommentRemove';
 
 interface PostCommentItemProps {
   postComment: PostComment;
 }
-export const PostCommentItem = ({postComment}: PostCommentItemProps) => (
-  <Box flexDirection="row" alignItems="center" mb="s16" gap="s12">
-    <ProfileAvatar imageURL={postComment.author.profileURL} />
-    <Box flex={1}>
-      <Text preset="paragraphSmall" bold>
-        {postComment.author.userName}
-      </Text>
-      <Text preset="paragraphSmall" color="gray1">
-        {postComment.message} - {postComment.formattedCreatedAt}
-      </Text>
-    </Box>
-  </Box>
-);
+export const PostCommentItem = ({postComment}: PostCommentItemProps) => {
+  const {mutate} = usePostCommentRemove();
+
+  function onConfirmRemove() {
+    Alert.alert('Deseja excluir o comentário?', 'pressione confirmar', [
+      {
+        text: 'Confirmar',
+        onPress: () => mutate(postComment.id),
+      },
+      {
+        text: 'Cancelar',
+        style: 'cancel',
+      },
+    ]);
+  }
+
+  return (
+    <Pressable onLongPress={onConfirmRemove}>
+      <Box flexDirection="row" alignItems="center" mb="s16" gap="s12">
+        <ProfileAvatar imageURL={postComment.author.profileURL} />
+        <Box flex={1}>
+          <Text preset="paragraphSmall" bold>
+            {postComment.author.userName}
+          </Text>
+          <Text preset="paragraphSmall" color="gray1">
+            {postComment.message} - {postComment.formattedCreatedAt}
+          </Text>
+        </Box>
+      </Box>
+    </Pressable>
+  );
+};
