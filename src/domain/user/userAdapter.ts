c2c37b9de4ddc855c6e2956c3a2048212fcde90c
Luiz Henrique <luizhjramos@outlook.com>
2024-07-18 11:27:52 -0300

    feat: add user domain adapter layer

---
+++ src/domain/user/userAdapter.ts
@@ -0,0 +1,14 @@
+import {User, UserAPI} from '@domain/user/userTypes';
+
+const toUser = (userAPI: UserAPI): User => ({
+  id: userAPI.id,
+  firstName: userAPI.first_name,
+  lastName: userAPI.last_name,
+  username: userAPI.username,
+  email: userAPI.email,
+  profileUrl: userAPI.profile_url,
+  fullName: userAPI.full_name,
+  isOnline: userAPI.is_online,
+});
+
+export const userAdapter = {toUser};
--- src/domain/user/userTypes.ts
+++ src/domain/user/userTypes.ts
@@ -1,12 +1,12 @@
 export interface User {
   id: number;
-  first_name: string;
-  last_name: string;
+  firstName: string;
+  lastName: string;
   username: string;
   email: string;
-  profile_url: string;
-  is_online: boolean;
-  full_name: string;
+  profileUrl: string;
+  isOnline: boolean;
+  fullName: string;
 }
 
 export interface UserAPI {
