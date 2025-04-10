ded395ab6cfe82ab6c346bc81e6bb63f42f17c06
Luiz Henrique <luizhjramos@outlook.com>
2025-04-10 12:30:01 -0300

    feat: add profile refetch feature

--- src/domain/user/useCases/useShowUser.ts
+++ src/domain/user/useCases/useShowUser.ts
@@ -3,10 +3,11 @@ import { useQuery } from '@tanstack/react-query';
 import { QueryKeys } from '@infra/types';
 
 export const useShowUser = (id: number) => {
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading, isError, refetch, isFetching } = useQuery({
     queryKey: [QueryKeys.useShowUserById, id],
     queryFn: () => userService.show(id),
+    staleTime: 1000 * 30,
   });
 
-  return { user: data, isLoading, isError };
+  return { user: data, isLoading, isError, refetch, isFetching };
 };
--- src/screens/app/MyProfileScreen/MyProfileScreen.tsx
+++ src/screens/app/MyProfileScreen/MyProfileScreen.tsx
@@ -8,28 +8,35 @@ import {AppScreenParams} from '@types';
 import {useShowUser} from '@domain/user/useCases/useShowUser';
 import {Box} from '@components/Box';
 import {ProfileAvatar} from '@components/ProfileAvatar';
+import {RefreshControl, ScrollView} from 'react-native';
 
 export const MyProfileScreen = ({route}: AppScreenParams<'ProfileScreen'>) => {
   const userId = route.params?.userId;
 
-  const {isError, isLoading, user} = useShowUser(userId);
+  const {isError, isLoading, user, isFetching, refetch} = useShowUser(userId);
 
   return (
-    <Screen canGoBack>
+    <Screen canGoBack flex={1}>
       {isLoading && <ActivityIndicator />}
       {isError && <Text> error ao carregar perfil do usuário</Text>}
       {user && (
-        <Box alignItems="center">
-          <ProfileAvatar
-            imageURL={user.profileUrl}
-            size={64}
-            borderRadius={24}
-          />
-          <Text preset="headingMedium" bold>
-            {user.fullName}
-          </Text>
-          <Text>@{user.username}</Text>
-        </Box>
+        <ScrollView
+          style={{flex: 1}}
+          refreshControl={
+            <RefreshControl refreshing={isFetching} onRefresh={refetch} />
+          }>
+          <Box alignItems="center" flex={1}>
+            <ProfileAvatar
+              imageURL={user.profileUrl}
+              size={64}
+              borderRadius={24}
+            />
+            <Text preset="headingMedium" bold>
+              {user.fullName}
+            </Text>
+            <Text>@{user.username}</Text>
+          </Box>
+        </ScrollView>
       )}
     </Screen>
   );
